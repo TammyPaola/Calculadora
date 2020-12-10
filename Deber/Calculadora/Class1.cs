b295e40ab2a68838ad50d072b224c77cca768311
TammyPaola <tpc.acosta@yavirac.edu.ec>
2020-12-10 17:52:47 -0500

    deber Calculadora

---
+++ Deber/Calculadora/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculadora
+{
+    public class Matematicas
+    {
+        public int Sumar(int sumando1, int sumando2)
+        {
+            return sumando1 + sumando2;
+        }
+        public int Restar(int sumando1, int sumando2)
+        {
+            return sumando1 - sumando2;
+        }
+        public int Multiplicar(int sumando1, int sumando2)
+        {
+            return sumando1 * sumando2;
+        }
+        public int Division(int sumando1, int sumando2)
+        {
+            return sumando1 / sumando2;
+        }
+        public int Potencia(int sumando1, int sumando2)
+        {
+            return sumando1 ^ sumando2;
+        }
+        public double Inversa(double sumando1)
+        {
+            return 1 / sumando1;
+        }
+        public double Raiz(double sumando1)
+        {
+             return Math.Sqrt(sumando1);
+        }
+    }
+}
---
+++ Deber/DeberApp/calculadora.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Calculadora;
+namespace DeberApp
+{
+    class calculadora
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deseas usar la calculadora?");
+            var opcion = Console.ReadLine();
+
+            while (opcion != "no")
+            {
+
+            Console.WriteLine("Bienvenido a mi calculadora");
+            Console.WriteLine("Que operacion quieres realizar");
+            var operacion = Console.ReadLine();
+            var calculadora = new Matematicas();
+
+                if (operacion == "+")
+
+                {
+                    Console.WriteLine("Ingresa un primer numero");
+                    var num1 = Console.ReadLine();
+                    Console.WriteLine("Ingresa un segundo numero");
+                    var num2 = Console.ReadLine();
+                    var respuesta = calculadora.Sumar(int.Parse(num1), int.Parse(num2));
+                    Console.WriteLine("la suma de los dos numeros es " + respuesta);
+                }
+                if (operacion == "-")
+                {
+                    Console.WriteLine("Ingresa un primer numero");
+                    var num1 = Console.ReadLine();
+                    Console.WriteLine("Ingresa un segundo numero");
+                    var num2 = Console.ReadLine();
+                    var respuesta = calculadora.Restar(int.Parse(num1), int.Parse(num2));
+                    Console.WriteLine("la resta de los dos numeros es " + respuesta);
+                }
+
+                if (operacion == "*")
+                {
+                    Console.WriteLine("Ingresa un primer numero");
+                    var num1 = Console.ReadLine();
+                    Console.WriteLine("Ingresa un segundo numero");
+                    var num2 = Console.ReadLine();
+                    var respuesta = calculadora.Multiplicar(int.Parse(num1), int.Parse(num2));
+                    Console.WriteLine("la multiplicacion de los dos numeros es " + respuesta);
+                }
+                if (operacion == "/")
+                {
+                    Console.WriteLine("Ingresa un primer numero");
+                    var num1 = Console.ReadLine();
+                    Console.WriteLine("Ingresa un segundo numero");
+                    var num2 = Console.ReadLine();
+                    var respuesta = calculadora.Division(int.Parse(num1), int.Parse(num2));
+                    Console.WriteLine("la division de los dos numeros es " + respuesta);
+                }
+                if (operacion == "potencia")
+                {
+                    Console.WriteLine("Ingresa un primer numero");
+                    var num1 = Console.ReadLine();
+                    Console.WriteLine("Ingresa un segundo numero");
+                    var num2 = Console.ReadLine();
+                    Console.WriteLine("el primer numero se tomara como exponente y el segundo como base");
+                    var respuesta = calculadora.Potencia(int.Parse(num1), int.Parse(num2));
+                    Console.WriteLine("la potencia es  " + respuesta);
+                }
+                if (operacion == "inversa")
+                {
+                    Console.WriteLine("Ingresa un numero");
+                    var num1 = Console.ReadLine();
+                    var respuesta = calculadora.Inversa(double.Parse(num1));
+                    Console.WriteLine("la inversa es " + respuesta);
+                }
+            if (operacion == "raiz")
+            {
+                Console.WriteLine("ingrese un numero ");
+                var num1 = Console.ReadLine();
+                var respuesta = calculadora.Raiz(double.Parse(num1));
+                Console.WriteLine("la raiz es " + respuesta);
+
+            }
+            
+                Console.WriteLine("Deseas volver a usar la calculadora ?");
+                 opcion= Console.ReadLine();
+            }
+
+        }
+
+        }
+    }
+
